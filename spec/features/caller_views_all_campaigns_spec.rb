37f16aec75137074880cae0871f606dfc2890532
Samantha Yeargin <slyeargin@gmail.com>
2014-09-16 19:57:33 -0500

    Began to implement campaign controller.

--- spec/features/caller_views_all_campaigns_spec.rb
+++ spec/features/caller_views_all_campaigns_spec.rb
@@ -1,9 +1,9 @@
 feature "Viewing campaigns" do
   scenario "Happy Path" do
-    pending "implementation"
     visit '/'
     click_on "View Campaigns"
     page.should have_content("Your Campaigns")
+    page.should have_content("You do not have any campaigns.")
     current_path.should == campaigns_path
   end
 
