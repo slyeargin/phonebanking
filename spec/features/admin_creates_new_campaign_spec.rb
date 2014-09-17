e493f534febfea224ca164d4b3df803595647e5b
Samantha Yeargin <slyeargin@gmail.com>
2014-09-16 20:17:29 -0500

    Completed add campaign path.

--- spec/features/admin_creates_new_campaign_spec.rb
+++ spec/features/admin_creates_new_campaign_spec.rb
@@ -7,6 +7,11 @@ feature "Adding a new campaign" do
     click_on "Create a Campaign"
     page.should have_content("Create a Campaign")
     current_path.should == new_campaign_path
+    fill_in "Name", with: "My Fantastic Campaign"
+    fill_in "Summary", with: "It's to let people know how awesome stuff is."
+    click_on "Create Campaign"
+    page.should have_content("The My Fantastic Campaign campaign has been created.")
+    current_path.should == campaigns_path
   end
 
 end
