53801a67ce8aa5af3fdec86cdee82dc041027719
Samantha Yeargin <slyeargin@gmail.com>
2014-09-18 09:55:10 -0500

    Tests for have_error now pass.

--- spec/features/user_signs_up_spec.rb
+++ spec/features/user_signs_up_spec.rb
@@ -29,20 +29,20 @@ feature "User signs up" do
     click_link "Sign up"
     click_button "Sign up"
     expect(page).to have_content("Please review the problems below")
-    # expect(page).to have_error("can't be blank", on: "Email")
-    # expect(page).to have_error("can't be blank", on: "Password")
+    expect(page).to have_error("can't be blank", on: "Email")
+    expect(page).to have_error("can't be blank", on: "Password")
   end
 
-  # scenario "Skipped password confirmation" do
-  #   visit '/'
-  #   click_link "Sign up"
-  #   expect(current_path).to eq new_user_registration_path
-  #   Capybara.exact = true
-  #   fill_in "* Email", with: "presidentcamacho@whitehouse.gov"
-  #   fill_in "* Password", with: "electrolytes"
-  #   Capybara.exact = false
-  #   click_button "Sign up"
-  #   expect(page).to have_error("doesn't match Password", on: "Password confirmation")
-  # end
+  scenario "Skipped password confirmation" do
+    visit '/'
+    click_link "Sign up"
+    expect(current_path).to eq new_user_registration_path
+    Capybara.exact = true
+    fill_in "* Email", with: "presidentcamacho@whitehouse.gov"
+    fill_in "* Password", with: "electrolytes"
+    Capybara.exact = false
+    click_button "Sign up"
+    expect(page).to have_error("doesn't match Password", on: "Password confirmation")
+  end
 
 end
